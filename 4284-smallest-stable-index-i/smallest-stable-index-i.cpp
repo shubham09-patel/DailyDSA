dcc953fc554c206830859d90a5bfbe352dbfb734
Shubham patel <167740978+shubham09-patel@users.noreply.github.com>
2026-09-05 01:55:53 +0530

    Time: 2 ms (29.26%) | Memory: 31.4 MB (44.16%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+         int n = nums.size();
+
+        vector<int> prefixMax(n);
+
+        vector<int> suffixMin(n);
+
+        // Build prefix maximum
+
+        prefixMax[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+
+            prefixMax[i] = max(prefixMax[i - 1], nums[i]);
+
+        }
+
+        // Build suffix minimum
+
+        suffixMin[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            suffixMin[i] = min(suffixMin[i + 1], nums[i]);
+
+        }
+
+        // Find smallest stable index
+
+        for (int i = 0; i < n; i++) {
+
+            int instability = prefixMax[i] - suffixMin[i];
+
+            if (instability <= k) {
+
+                return i;
+
+            }
+
+        }
+
+        return -1;
+
+
+    }
+};
