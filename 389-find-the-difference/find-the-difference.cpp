ad40f504811dbaab465a330040d1142ffc363be9
Shubham patel <167740978+shubham09-patel@users.noreply.github.com>
2026-09-26 20:01:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.84%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int freq[26]= {0};
+        for(char c : s){
+            freq[c - 'a']++;
+        }
+        for(char c : t){
+            freq[c - 'a']--;
+        }
+
+        for(int i = 0; i<26; i++){
+            if(freq[i] < 0){
+                return 'a' + i;
+            }
+            
+        }
+        
+        return ' ';
+    }
+};
